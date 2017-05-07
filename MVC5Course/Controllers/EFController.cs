cf0437b4cd49ae528bc31ed8bb6a668431e3ffeb
TsaiChappie <aboutskygooogle@gmail.com>
2017-05-07 12:28:05 +0800

    Cerate EFcontroller

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        FabricsEntities1 db = new FabricsEntities1();
+        // GET: EF
+        public ActionResult Index()
+        {
+            var all = db.Product.AsQueryable();//.AsQueryable() 不會立刻執行 延遲執行
+            var data = all.Where(p => p.Active == true && p.ProductName.Contains("Black"))
+                        .OrderByDescending(p => p.ProductId);
+
+
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var data = db.Product.Find(id);
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.Product.Find(id);
+                item.Active = data.Active;
+                item.Price = data.Price;
+                item.ProductName = data.ProductName;
+                item.Stock = data.Stock;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item= db.Product.Find(id);
+            db.Product.Remove(item);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
