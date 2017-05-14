dcc23906bf70efde97b434fd24aa72f1ff394fd7
chappie <aboutskygooogle@gmail.com>
2017-05-14 17:20:34 +0800

    練習 Html.Action 與 Html.Partial 用法

---
+++ MVC5Course/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private FabricsEntities1 db = new FabricsEntities1();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            var client = db.Client.Include(c => c.Occupation);
+            return View(client.ToList());
+        }
+        
+    }
+}
